73c4100e40346d3ce3cf9cf4577f6ee66cf0fb88
Axel Montes de Oca <axelmontes@hotmail.com>
2022-03-22 09:04:12 -0600

    Relocate

--- 02-intro-javascript/src/bases/02-string-concatenation.js
+++
@@ -1,7 +0,0 @@
-const nombre = "Axel";
-const apellido = "Montes de Oca";
-
-//const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${nombre} ${apellido}`;
-
-console.log(nombreCompleto);
--- 05-hook-app/src/HookApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HookApp() {
-    return (
-        <div>
-            <h1>Hola Mundo</h1>
-        </div>
-    )
-}
